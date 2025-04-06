c6a5c8ce2555db9f318b76e111534704a4e70584
RimonChakma <rimon.chakma880@gmail.com>
2025-04-06 21:22:08 +0600

    create TextField

--- lib/task_create_screen.dart
+++ lib/task_create_screen.dart
@@ -6,7 +6,18 @@ class TaskCreateScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(title: Text("Create Screen"),centerTitle: true,),
+      body: Padding(padding: EdgeInsets.all(10),child: Column(
+        children: [
+          TextField(decoration: InputDecoration(
+            hintText: "name"
+          ),),
+          SizedBox(height: 10,),
+          TextField(decoration: InputDecoration(
+            hintText: "age"
+          ),)
+        ],
+      ),),
     );
   }
 }
