636ad420f05ff5e8a00939bfbee48a12e80cc744
RimonChakma <rimon.chakma880@gmail.com>
2025-04-07 20:18:28 +0600

    add button and addController

--- lib/task_create_screen.dart
+++ lib/task_create_screen.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
 class TaskCreateScreen extends StatelessWidget {
-  const TaskCreateScreen({super.key});
+   TaskCreateScreen({super.key});
 
+  TextEditingController addController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Create Screen"),centerTitle: true,),
       body: Padding(padding: EdgeInsets.all(10),child: Column(
         children: [
-          TextField(decoration: InputDecoration(
+          TextField(
+            controller: addController,
+            decoration: InputDecoration(
             hintText: "name"
           ),),
           SizedBox(height: 10,),
-          TextField(decoration: InputDecoration(
+          TextField(
+            controller: addController,
+            decoration: InputDecoration(
             hintText: "age"
-          ),)
+          ),),
+          SizedBox(height: 10,),
+          ElevatedButton(onPressed: (){}, child: Text("saves")),
         ],
       ),),
     );
